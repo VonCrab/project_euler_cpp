e0152051a5abae2d47f5d42f94bf05e7f5fb241b
VonCrab <lukepensiero@gmail.com>
2020-05-03 15:21:03 -0400

    solved problem 1

--- problems/solution_1/solution_1.cpp
+++ problems/solution_1/solution_1.cpp
@@ -14,19 +14,11 @@
 #   define ASSERT(condition, message) do { } while (false)
 #endif
 
-auto multiples(int max) {
+auto multiples_of_3_and_5(int max) {
     auto sum = 0;
 
     for (auto i = 0; i < max; i++) {
-        bool mutliple_of_threes = (i % 3) == 0;
-        bool multiple_of_fives = (i % 5) == 0;
-
-        if (mutliple_of_threes && multiple_of_fives) {
-            sum = sum + i;
-            continue;
-        } else if (multiple_of_fives) {
-            sum = sum + i;
-        } else if (mutliple_of_threes) {
+        if (i % 3 == 0 || i % 5 == 0) {
             sum = sum + i;
         }
     }
@@ -34,34 +26,15 @@ auto multiples(int max) {
     return sum;
 }
 
-auto multiplesv2(int max) {
-    auto sum = 0;
-    auto i = 1;
-    bool multiples_remaining = true;
-
-    while (multiples_remaining) {
-        auto multiple_three = i * 3;
-        auto multiple_five = i * 5;
-
-        if (multiple_three == multiple_five) {
-            sum += multiple_three;
-            i += 1;
-        } else if (multiple_five ) {
-            /* code */
-        }
-        
-    }
-}
-
 int main() {
-    auto multiples_10 = multiples(10);
-    ASSERT(multiples_10 == 23, "multiples returned " << multiples_10 << ", but were expecting 23");
+    // auto multiples_10 = multiples_of_3_and_5(10);
+    // ASSERT(multiples_10 == 23, "multiples returned " << multiples_10 << ", but were expecting 23");
 
-    auto multiples_20 = multiples(20);
-    ASSERT(multiples_20 == 78, "multiples returned " << multiples_20 << ", but were expecting 78");
+    // auto multiples_20 = multiples_of_3_and_5(20);
+    // ASSERT(multiples_20 == 78, "multiples returned " << multiples_20 << ", but were expecting 78");
 
-    auto multiples_100 = multiples(100);
-    ASSERT(multiples_100 == 78, "multiples returned " << multiples_100 << ", but were expecting 78");
+    // auto multiples_100 = multiples_of_3_and_5(1000);
+    // ASSERT(multiples_100 == 233168, "multiples returned " << multiples_100 << ", but were expecting 233168");
 
-    return 0;
+    return multiples_of_3_and_5(1000);
 }
